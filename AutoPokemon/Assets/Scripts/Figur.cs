bab54393ec1d1b580da55416366b052062622a43
Santhosh Udayakumar <santhoshu02@gmail.com>
2022-03-22 08:19:00 +0100

    s
    s2

--- AutoPokemon/Assets/Scripts/Figur.cs
+++ AutoPokemon/Assets/Scripts/Figur.cs
@@ -7,12 +7,15 @@ public class Figur : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int liv;
     }
+    public void Pokemon(int liv) {
 
+    }
     // Update is called once per frame
     void Update()
     {
         
+
     }
 }
--- AutoPokemon/Assets/Scripts/Pokemonkode.cs
+++ AutoPokemon/Assets/Scripts/Pokemonkode.cs
@@ -7,12 +7,16 @@ public class Pokemonkode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Figur Pikachu = new Figur();
+        Pikachu.Pokemon(300);
+        System.Console.WriteLine(Pikachu);
 
+    }
+  
     // Update is called once per frame
     void Update()
     {
-        
+
+        System.Console.WriteLine("s");
     }
 }
